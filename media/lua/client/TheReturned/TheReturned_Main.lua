07b5c3b7cd4913e961bae38d05d1f01640c342a9
quarantin <codelorme@gmail.com>
2022-01-10 17:36:05 +0100

    Initial commit

---
+++ media/lua/client/TheReturned/TheReturned_Main.lua
@@ -0,0 +1,127 @@
+local TheReturned = {}
+
+TheReturned.modId = 'TheReturned'
+TheReturned.trait = 'Returned'
+TheReturned.profession = 'returner'
+
+function TheReturned.getBoostLevel(level)
+
+	if level == 0 then
+		return 1
+	end
+
+	if level % 2 == 0 then
+		return level - 1
+	end
+
+	return level
+end
+
+function TheReturned.getPlayerData(player)
+
+	local playerData = {}
+
+	playerData.perks = {}
+	playerData.traits = {}
+	playerData.profession = player:getDescriptor():getProfession()
+
+	local xp = player:getXp()
+	local perks = PerkFactory.PerkList
+	local traits = player:getTraits()
+
+	for i = 0, perks:size() - 1 do
+
+		local perk = perks:get(i)
+		local perkName = PerkFactory.getPerkName(perk)
+		local perkLevel = player:getPerkLevel(perk)
+		local boostLevel = TheReturned.getBoostLevel(perkLevel)
+
+		playerData.perks[perkName] = {
+			level = perkLevel,
+			minLevel = boostLevel,
+			maxLevel = boostLevel + 1,
+			boost = xp:getMultiplier(perk),
+		}
+	end
+
+	for i = 0, traits:size() - 1 do
+		table.insert(playerData.traits, traits:get(i))
+	end
+
+	return playerData
+end
+
+function TheReturned.setPlayerData(player, playerData)
+
+	player:getDescriptor():setProfession(playerData.profession)
+
+	local xp = player:getXp()
+
+	for perkName, perkData in pairs(playerData.perks) do
+
+		local perk = PerkFactory.getPerkFromName(perkName)
+
+		for level = 1, perkData.level do
+			local perkLevel = player:getPerkLevel(perk)
+			if level > perkLevel then
+				player:LevelPerk(perk)
+				xp:setXPToLevel(perk, perkData.level)
+			end
+		end
+
+		xp:addXpMultiplier(perk, perkData.boost, perkData.minLevel, perkData.maxLevel)
+	end
+
+	local traits = player:getTraits()
+	traits:clear()
+	for _, trait in pairs(playerData.traits) do
+		traits:add(trait)
+	end
+end
+
+function TheReturned.DoProfessions()
+
+	TraitFactory.addTrait(TheReturned.trait, getText("UI_trait_Returned"), 0, getText("UI_trait_ReturnedDesc"), true, false)
+
+	local traits = TraitFactory.getTraits()
+	for i = 0, traits:size() - 1 do
+		TraitFactory.setMutualExclusive(TheReturned.trait, traits:get(i):getType())
+	end
+
+	local prof = ProfessionFactory.addProfession(TheReturned.profession, getText("UI_prof_returner"), 'profession_' .. TheReturned.profession, 0)
+	prof:addFreeTrait(TheReturned.trait)
+end
+
+BaseGameCharacterDetails.originalDoProfessions = BaseGameCharacterDetails.DoProfessions
+function BaseGameCharacterDetails.DoProfessions()
+	TheReturned.DoProfessions()
+	BaseGameCharacterDetails.originalDoProfessions()
+end
+
+function TheReturned.OnGameBoot()
+	ProfessionFactory.Reset()
+	BaseGameCharacterDetails.DoProfessions()
+end
+
+function TheReturned.OnCreatePlayer(playerId, player)
+
+	local modData = ModData.get(TheReturned.modId)
+	if modData then
+
+		local playerData = modData[getCurrentUserSteamID()]
+		if playerData and player:HasTrait(TheReturned.trait) then
+			TheReturned.setPlayerData(player, playerData)
+		end
+
+	end
+end
+
+function TheReturned.OnPlayerDeath(player)
+	local modData = ModData.getOrCreate(TheReturned.modId)
+	modData[getCurrentUserSteamID()] = TheReturned.getPlayerData(player)
+	ModData.transmit(TheReturned.modId)
+end
+
+Events.OnGameBoot.Add(TheReturned.OnGameBoot)
+Events.OnCreatePlayer.Add(TheReturned.OnCreatePlayer)
+Events.OnPlayerDeath.Add(TheReturned.OnPlayerDeath)
